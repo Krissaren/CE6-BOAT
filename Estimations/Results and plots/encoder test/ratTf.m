20199abaf2477b76aff1943c12e850a2bcfc729d
Morten <mrnabe19@student.aau.dk>
2022-03-24 10:02:23 +0100

    Results and plots of EC motor estimations

---
+++ Estimations/Results and plots/encoder test/ratTf.m
@@ -0,0 +1,35 @@
+clear
+clc
+
+k =20000;
+tau= 1.35;
+s = tf('s');
+G = k/(tau*s+1)
+[B, t] = step(G);
+
+A1=load('rat1.txt');
+A2=load('rat2.txt');
+A3=load('rat5.txt');
+
+figure(1)
+plot((A1(:,2)-A1(1,2))/1000,A1(:,1))
+hold on
+plot(t,B)
+xlabel('Time [s]') 
+ylabel('Encoder value') 
+grid on
+xlim([0 9])
+hold off
+
+figure(2)
+plot((A2(:,2)-A2(1,2))/1000,A2(:,1)/5000)
+hold on
+plot((A1(:,2)-A1(1,2))/1000,A1(:,1)/20000)
+plot((A3(:,2)-A3(1,2))/1000,A3(:,1)/40000)
+xlabel('Time [s]') 
+ylabel('Amplitude') 
+grid on
+xlim([0 7])
+ylim([0 1.2])
+legend({'Step = 5000','Step = 20000', 'Step = 40000'},'Location','southeast')
+hold off
---
+++ Estimations/Results and plots/encoder test/readmeThrot.txt
@@ -0,0 +1,8 @@
+throt1 +20000
+throt2 +5000
+throt3 -20000
+throt4 -5000
+throt5 +40000
+
+min 0
+max 69500
