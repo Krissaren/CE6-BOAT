3f75d15d22d6238454c7818605cd38795fe7926d
Jonas <jonasemil0305@gmail.com>
2022-04-19 12:33:31 +0200

    hurlimhej with vmass and vinertia

---
+++ Estimations/Boat test 2/Inertia/virtualInertia.m
@@ -0,0 +1,56 @@
+clear
+clc
+close all
+
+txtname = ('throt12967rud60000.txt');
+f = fopen(txtname);
+unParsedNMEAdata = fread(f);
+pnmea = nmeaParser("MessageIDs",["VTG","GGA"]);
+[VTG, GGA] = pnmea(unParsedNMEAdata);
+
+Time = [GGA(:).UTCTime];
+%ms = milliseconds(Time - Time(1));
+secs = seconds(Time - Time(1));
+%secs(end+1) = secs(end)+0.05; %Time vector is sometimes is 1 smaller, so here's fix
+%secs(end) = []; %Time vector is sometimes is 1 larger, so here's fix
+AngPos = [VTG(:).TrueCourseAngle];
+
+m = length(AngPos);
+for i = 1:1:m-1
+    if (AngPos(i)-AngPos(i+1))>350
+        AngPos(i+1) = AngPos(i+1)+360;
+    end
+end 
+AngPos = movmean(AngPos,20);
+
+for i = 1:length(AngPos)-1 %Angular velocity. Could use gradient()
+    AngSpeed(i) = (AngPos(i+1)-AngPos(i))/(secs(i+1)-secs(i));
+end
+
+for i = 1:length(AngSpeed)-1 %Angular acceleration
+    AngAcc(i) = (AngSpeed(i+1)-AngSpeed(i))/(secs(i+1)-secs(i));
+end
+
+throtConst = 0.00036320754;
+rudderEnc = 60000;
+rudderAng = throtConst * rudderEnc;
+
+throttleEnc = 13000;
+tau = throttleEnc*0.1747-1631.5
+
+readAccel = (14.39-7.87)/(abs(28.45-28.55))
+
+inertia = tau/readAccel
+
+%AngSpeed = gradient(AngPos);
+
+plot(secs(1:744),AngSpeed)
+%plot(secs(1:743),AngAcc)
+hold on
+grid
+ylabel = ('Degrees [°]');
+xlabel = ('Time [s]');
+legend = (txtname);
+%plot(secs,AngPos)
+%plot(secs(1:743),AngAcc)
+hold off
---
+++ Estimations/Boat test 2/VirtualMass/virtualmass.m
@@ -0,0 +1,37 @@
+clear
+clc
+close all
+
+f = fopen('throt11866E.txt');
+unParsedNMEAdata = fread(f);
+pnmea = nmeaParser("MessageIDs",["VTG","GGA"]);
+[VTG, GGA] = pnmea(unParsedNMEAdata);
+
+Time = [GGA(:).UTCTime];
+%ms = milliseconds(Time - Time(1));
+secs = seconds(Time - Time(1));
+%secs(end+1) = secs(end)+0.05; %Time vector is sometimes is 1 smaller, so here's fix
+%secs(end) = []; %Time vector is sometimes is 1 larger, so here's fix
+Speed = [VTG(:).GroundSpeed];
+TOPFART = max(abs(Speed))
+
+Accel = gradient(Speed)./0.050;
+SpeedFiltered = movmean(Speed,10);
+AccelFiltered = gradient(SpeedFiltered)./0.050;
+
+readAccel = (2.94519-2.38188)/(abs(18.1-18.15));
+
+max(abs(Accel));
+virtualMass = 500/readAccel % 500 N at 12000 encoder value
+%virtualMass = 995/readAccel % 1000 N at 15000 encoder value
+figure(1)
+plot(secs,Speed)
+hold on;
+xlabel('Time [s]')
+%ylabel('Acceleration [m/s^2]')
+ylabel('Velocity [m/s]')
+legend('throt11984W')
+grid
+%plot(secs,AccelFiltered)
+hold off;
+%ylim([0 0.1])
